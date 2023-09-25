23bb143ae924f91b2966b631b147df2e4e5b6d84
pauljeffrey <jeffreyotoibhi@rocketmail.com>
2023-09-25 14:39:35 +0100

    edited code in prepare.py and train.py

--- data/openwebtext/prepare.py
+++ data/openwebtext/prepare.py
@@ -19,7 +19,7 @@ num_proc_load_dataset = num_proc
 if __name__ == '__main__':
     # takes 54GB in huggingface .cache dir, about 8M documents (8,013,769)
     #dataset = load_dataset("openwebtext", num_proc=num_proc_load_dataset)
-    data_files = ["data/train-00001-of-00035-0ffa1b2c1533e462.parquet", "train-00002-of-00035-8d4d29f0bb986f30.parquet"]
+    data_files = ["data/train-00001-of-00035-0ffa1b2c1533e462.parquet"]
     # # ,"train-00002-of-00035-8d4d29f0bb986f30.parquet", "train-00023-of-00035-1751103bdc6eb74c.parquet","train-00031-of-00035-e8233b95e5b92059.parquet", "train-00029-of-00035-4fda4ad62c4ffb34.parquet",
     # # "train-00022-of-00035-64d5581b0d8c4437.parquet"]
     # #https://huggingface.co/datasets/vietgpt/the_pile_openwebtext2/blob/main/data/
